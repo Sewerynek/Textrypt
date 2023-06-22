a3f01f4b3b980499563185ac7057b6de33152c1e
Matossy <134222272+Matossy@users.noreply.github.com>
2023-06-22 21:35:28 +0300

    Add files via upload

---
+++ TextEncryptionApp/TextEncryptionApp/src/com/textencryptionapp/algorithms/MorseCode.java
@@ -0,0 +1,59 @@
+package com.textencryptionapp.algorithms;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MorseCode implements EncryptionAlgorithm {
+
+    private static final Map<Character, String> morseCodeMap;
+
+    static {
+        morseCodeMap = new HashMap<>();
+        morseCodeMap.put('A', ".-");
+        morseCodeMap.put('B', "-...");
+        morseCodeMap.put('C', "-.-.");
+        morseCodeMap.put('D', "-..");
+        morseCodeMap.put('E', ".");
+        morseCodeMap.put('F', "..-.");
+        morseCodeMap.put('G', "--.");
+        morseCodeMap.put('H', "....");
+        morseCodeMap.put('I', "..");
+        morseCodeMap.put('J', ".---");
+        morseCodeMap.put('K', "-.-");
+        morseCodeMap.put('L', ".-..");
+        morseCodeMap.put('M', "--");
+        morseCodeMap.put('N', "-.");
+        morseCodeMap.put('O', "---");
+        morseCodeMap.put('P', ".--.");
+        morseCodeMap.put('Q', "--.-");
+        morseCodeMap.put('R', ".-.");
+        morseCodeMap.put('S', "...");
+        morseCodeMap.put('T', "-");
+        morseCodeMap.put('U', "..-");
+        morseCodeMap.put('V', "...-");
+        morseCodeMap.put('W', ".--");
+        morseCodeMap.put('X', "-..-");
+        morseCodeMap.put('Y', "-.--");
+        morseCodeMap.put('Z', "--..");
+    }
+
+    @Override
+    public String encrypt(String plaintext) {
+        StringBuilder ciphertext = new StringBuilder();
+        for (char letter : plaintext.toUpperCase().toCharArray()) {
+            String morseCode = morseCodeMap.get(letter);
+            if (morseCode != null) {
+                ciphertext.append(morseCode).append(" ");
+            } else {
+                ciphertext.append(letter);
+            }
+        }
+        return ciphertext.toString().trim();
+    }
+
+    @Override
+    public String decrypt(String ciphertext) {
+        // Decryption
+        return "";
+    }
+}
